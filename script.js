a4b85f4f9c733838f54731467e30c5872dc63f07
Maxi-raph <102929251+Maxi-raph@users.noreply.github.com>
2025-09-19 17:12:00 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+const navbar = document.querySelector('.navbar')
+const linksContainer = document.querySelector('.link-contain')
+const links = document.querySelectorAll('.links')
+const hamburger = document.querySelector('.hamburger-icon')
+links.forEach(li => {
+  li.addEventListener('click', (e) => {
+    let id = e.target.dataset.name
+    let section = document.getElementById(id)
+    section.scrollIntoView({ behaviour: 'smooth' })
+    if (linksContainer.classList.contains('max-sm:gap-0')) {
+      linksContainer.classList.add('max-sm:hidden')
+      linksContainer.classList.remove('flex')
+      hamburger.classList.remove("max-sm:bg-transparent");
+      hamburger.classList.remove("max-sm:before:top-0");
+      hamburger.classList.remove("max-sm:before:[rotate:45deg]");
+      hamburger.classList.remove("max-sm:after:top-0");
+      hamburger.classList.remove("max-sm:after:[rotate:-45deg]");
+    }
+    if (linksContainer.classList.contains('max-xs:gap-0')) {
+      linksContainer.classList.add('max-xs:hidden')
+      linksContainer.classList.remove('flex')
+      hamburger.classList.remove("max-xs:bg-transparent");
+      hamburger.classList.remove("max-xs:before:top-0");
+      hamburger.classList.remove("max-xs:before:[rotate:45deg]");
+      hamburger.classList.remove("max-xs:after:top-0");
+      hamburger.classList.remove("max-xs:after:[rotate:-45deg]");
+    }
+  })
+})
+hamburger.addEventListener('click', () => {
+  if (linksContainer.classList.contains('max-xs:hidden') || linksContainer.classList.contains('max-sm:hidden')) {
+    linksContainer.classList.add('flex')
+    linksContainer.classList.remove('max-xs:hidden')
+    linksContainer.classList.remove('max-sm:hidden')
+  } else {
+    linksContainer.classList.add('max-xs:hidden')
+    linksContainer.classList.add('max-sm:hidden')
+    linksContainer.classList.remove('flex')
+  }
+  hamburger.classList.toggle("max-sm:bg-transparent");
+  hamburger.classList.toggle("max-xs:bg-transparent");
+  hamburger.classList.toggle("max-sm:before:top-0");
+  hamburger.classList.toggle("max-xs:before:top-0");
+  hamburger.classList.toggle("max-sm:before:[rotate:45deg]");
+  hamburger.classList.toggle("max-xs:before:[rotate:45deg]");
+  
+  hamburger.classList.toggle("max-sm:after:top-0");
+  hamburger.classList.toggle("max-xs:after:top-0");
+  hamburger.classList.toggle("max-sm:after:[rotate:-45deg]");
+  hamburger.classList.toggle("max-xs:after:[rotate:-45deg]");
+})
+
